8efc1c22c10b2b87044c611b7e363ba598179c8b
samu112 <lerantsamuel@gmail.com>
2023-02-16 19:16:48 +0100

    minor template fixes

--- pages/global/page_template.php
+++ pages/global/page_template.php
@@ -1,16 +1,23 @@
+<!--Head-->
 <?php
 $page_title = 'New Page';
 include($_SERVER['DOCUMENT_ROOT'].'/pages/global/header.php');
 ?>
+<!--Head-->
+
+
+<!--Body-->
 
 
 
 
 
 
+<!--Body-->
 
 
-
+<!--Footer-->
 <?php
 include($_SERVER['DOCUMENT_ROOT'].'/pages/global/footer.php');
 ?> 
+<!--Footer-->
--- pages/page2.php
+++ pages/page2.php
@@ -1,16 +1,23 @@
+
+ <!--Head-->
 <?php
-$page_title = 'teszt2';
+$page_title = 'Test 2';
 include($_SERVER['DOCUMENT_ROOT'].'/pages/global/header.php');
 ?>
+ <!--Head-->
+
+<!--Body-->
+
+Test
 
 
-Test2
 
 
 
+<!--Body-->
 
-
-
+<!--Footer-->
 <?php
 include($_SERVER['DOCUMENT_ROOT'].'/pages/global/footer.php');
 ?> 
+<!--Footer-->
