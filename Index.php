c6939f979404e11c543394b26a0ddefe4c9bb583
samu112 <lerantsamuel@gmail.com>
2023-02-17 15:06:20 +0100

    loginPage layout fixed

--- Index.php
+++ Index.php
@@ -1,39 +1,24 @@
 
- <!--Head-->
+
 <?php
 $page_title = 'Login';
 include($_SERVER['DOCUMENT_ROOT'].'/pages/global/header.php');
 ?>
- <!--Head-->
 
-<!--Body-->
 
+
+<div class="flex-grow-1 logindiv">
     <form action="/action_page.php" method="post">
-        <div class="container h-100">
-            <div class="row h-100 justify-content-center align-items-center">
-                <form class="col-12">
-                <div class="form-group text-center">
-                    <label class="form-label" for="form2Example2">Password</label>
-                    <input type="password" id="form2Example2" class="form-control" />
-                </div>
-                <div class="form-group text-center">
-                    <button type="button" class="btn btn-primary btn-block mb-4">Sign in</button>
-                </div>
-                </form>   
-            </div>
-        </div>
+        <input type="text" id="form2Example2" placeholder="Password" class="form-control loginInputText text-center" />
+        <button type="button" class="btn btn-warning btn-block mb-4">Sign in</button>
     </form>
+</div>
 
 
 
 
 
 
-
-<!--Body-->
-
-<!--Footer-->
 <?php
 include($_SERVER['DOCUMENT_ROOT'].'/pages/global/footer.php');
 ?> 
-<!--Footer-->
