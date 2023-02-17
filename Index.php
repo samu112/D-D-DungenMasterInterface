00ba862b26e0a5be9b033b70c81d6e012d5a9987
samu112 <lerantsamuel@gmail.com>
2023-02-17 19:45:55 +0100

    minor fixes(name of the website, formatting...)

--- Index.php
+++ Index.php
@@ -1,25 +1,13 @@
-
-
 <?php
 $page_title = 'Login';
 include($_SERVER['DOCUMENT_ROOT'].'/pages/global/header.php');
 ?>
-
-
-
-<div class="flex-grow-1 logindiv">
-    <form action="/action_page.php" method="post">
-        <input type="text" id="form2Example2" placeholder="Password" class="form-control loginInputText text-center" />
-        <button type="button" class="btn btn-warning btn-block mb-4">Sign in</button>
-    </form>
-</div>
-
-
-
-
-
-
-
+        <div class="flex-grow-1 logindiv">
+            <form action="/action_page.php" method="post">
+                <input type="text" id="form2Example2" placeholder="Password" class="form-control loginInputText text-center" />
+                <button type="button" class="btn btn-warning btn-block mb-4">Sign in</button>
+            </form>
+        </div>
 <?php
 include($_SERVER['DOCUMENT_ROOT'].'/pages/global/footer.php');
 ?> 
