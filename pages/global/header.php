0c09681527c26a67174096b74594033a0b6f9665
samu112 <lerantsamuel@gmail.com>
2023-02-20 22:35:45 +0100

    Header modified to allow extra header elementsa

--- pages/global/header.php
+++ pages/global/header.php
@@ -5,5 +5,8 @@
         <title>D&D Help Desk - <?php echo $page_title; ?></title>
         <link href="\bootstrap-5.3.0-alpha1-dist\css\bootstrap.min.css" rel="stylesheet">
         <link href="\css\style.css" rel="stylesheet">
+        <?php if (isset($extraHeaders)==true){
+            echo $extraHeaders;
+        }?>
     </head>
     <body>
