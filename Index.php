07f6040a55bae01ec34aa7ad4fd90ff822aec38d
elodborbath <elodborbath@gmail.com>
2023-02-20 22:34:29 +0100

    Stuff

--- Index.php
+++ Index.php
@@ -3,9 +3,9 @@ $page_title = 'Login';
 include($_SERVER['DOCUMENT_ROOT'].'/pages/global/header.php');
 ?>
         <div class="flex-grow-1 logindiv">
-            <form action="/action_page.php" method="post">
+            <form action="/pages/AfterLogin.php" method="post">
                 <input type="text" id="form2Example2" placeholder="Password" class="form-control loginInputText text-center" />
-                <button type="button" class="btn btn-warning btn-block mb-4">Sign in</button>
+                <button type="submit" class="btn btn-warning btn-block mb-4">Sign in</button>
             </form>
         </div>
 <?php
--- pages/AfterLogin.php
+++ pages/AfterLogin.php
@@ -2,25 +2,33 @@
  <?php
 $page_title = 'Test 2';
 include($_SERVER['DOCUMENT_ROOT'].'/pages/global/header.php');
-include 'CSS/style.css';
 ?>
  <!--Head-->
 
  <!--Body-->
  <script src="\bootstrap-5.3.0-alpha1-dist\js\bootstrap.bundle.min.js"></script>
 
-<div id="footer">
-    <div id="footerleftpane" runat="server">
-        <a>Global settings</a>
-    </div>
-    <div id="footerMidPane" runat="server">
-        <a>Session code</a>
-    </div>
-    <div id="footerRightPane" runat="server">
-        <a>Reset</a>
-    </div> 
+ <!--Navbar-->
+ <nav class="navbar navbar-expand-lg navbar-light bg-light justify-content-center">
+ <form class="form-inline">
+    <button class="btn btn-outline-success" type="button">Global settings</button>
+    <button class="btn btn-outline-success disabled" type="button">Session code: +++++++++</button>
+    <button class="btn btn-sm btn-outline-secondary" type="button">Reset</button>
+  </form>
+</nav>
+ <!--Navbar-->
+
+ <!--Dropup button-->
+ <div class="menu" style="position: fixed;bottom: 10px;right: 10px;">
+  <div class="item"><span>Twitter</span></div>
+  <div class="item"><span>Instagram</span></div>
+  <div class="item"><span>Flickr</span></div>
+  <div class="item"><span>Behance</span></div>
+  <div class="item"><span>MixCloud</span></div>
+  <div class="label">Follow Me</div>
 </div>
 
+<!--Dropup button-->
 
 <!--Body-->
 
