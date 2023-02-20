41570308897cb1cb7905db78c1a3e23a3005dc96
elodborbath <elodborbath@gmail.com>
2023-02-20 23:32:32 +0100

    Dropup fix again

--- pages/AfterLogin.php
+++ pages/AfterLogin.php
@@ -24,9 +24,9 @@ include($_SERVER['DOCUMENT_ROOT'].'/pages/global/header.php');
 
  <!--Dropup button-->
  <div class="menuX" style="text-align: center;">
-  <div class="itemX"><button type="button" class="btn btn-primary btn-lg">Players</button></div>
-  <div class="itemX"><button type="button" class="btn btn-primary btn-lg">Items</button></div>
-  <div class="itemX"><button type="button" class="btn btn-primary btn-lg">Monsters</button></div>
+  <div class="itemX"><button type="button" class="btn btn-primary btn-lg" style="min-width: 100%;">Players</button></div>
+  <div class="itemX"><button type="button" class="btn btn-primary btn-lg" style="min-width: 100%;">Items</button></div>
+  <div class="itemX"><button type="button" class="btn btn-primary btn-lg" style="min-width: 100%;">Monsters</button></div>
   <svg class="labelX" xmlns="http://www.w3.org/2000/svg" width="5vw" height="5vh" fill="currentColor" class="bi bi-plus-square" viewBox="0 0 16 16">
   <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
   <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
