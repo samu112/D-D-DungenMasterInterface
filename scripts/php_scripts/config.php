78cecc837cc63f437280c4a968fb81e933c4e018
samu112 <lerantsamuel@gmail.com>
2023-02-23 02:36:03 +0100

    some comment added to config and logout php

--- scripts/php_scripts/config.php
+++ scripts/php_scripts/config.php
@@ -1,6 +1,17 @@
 <?php
 //Tutorial: https://www.tutorialrepublic.com/php-tutorial/php-mysql-login-system.php
 
+/*
+    //Query example:
+    // Select query
+        $sql = 'SELECT * FROM users';
+        if ($result = $link->query($sql)) {
+            while ($data = $result->fetch_object()) {
+                $users[] = $data;
+            }
+        }
+*/
+
 $host = 'db';
 
 // Database use name
--- scripts/php_scripts/logout.php
+++ scripts/php_scripts/logout.php
@@ -1,6 +1,8 @@
 <?php
+    //usage example: <a href="\scripts\php_scripts\logout.php">Logout</a>
     session_start();
     session_destroy();
+    //Redirect to main page
     header('Location: /index.php');
     exit;
 ?>
